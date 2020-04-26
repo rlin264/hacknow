d218f2fb4a8c54a625a87c960a2c734479c64ee3
selenaqian <selenaq2010@gmail.com>
2020-04-26 02:42:47 -0400

    got nested tnavigators to work but it breaks the 'buttons' on the communities screen - decided to move map to bottom tab; need change Home to say Log out and hide on main

--- frontend/navigation/AppNavigator_v2.js
+++ frontend/navigation/AppNavigator_v2.js
@@ -1,39 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Drawer, DrawerItem, Layout, Text, Button } from '@ui-kitten/components';
 
-import CommunitiesNav from '../screens/Communities.js';
 import Login from '../components/Drawer/Login/Login.js';
 import Tabs from './TabNavigator.js';
 
 const Stack = createStackNavigator();
 
-export const mapScreen = () => (
-  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text category='h1'>MAP</Text>
-  </Layout>
-);
-
-const ph1Screen = () => (
-  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text category='h1'>Placeholder1</Text>
-  </Layout>
-);
-
-const ph2Screen = () => (
-  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text category='h1'>Placeholder2</Text>
-  </Layout>
-);
-
 export const AppNavigator = () => (
 <NavigationContainer>
   <Stack.Navigator>
     <Stack.Screen name='Home' component={ Login }/>
-    <Stack.Screen name='Your Communities' component={ CommunitiesNav }/>
-    <Stack.Screen name='Map' component={ mapScreen }/>
+    <Stack.Screen name='Your Communities' component={ Tabs }/>
   </Stack.Navigator>
   </NavigationContainer>
 );
--- frontend/navigation/TabNavigator.js
+++ frontend/navigation/TabNavigator.js
@@ -1,11 +1,16 @@
 //first inside navigator - tab containing stack, screen, screen
 
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Layout, Text} from '@ui-kitten/components';
+
+import CommunitiesNav from '../screens/Communities.js';
 
 const Tab = createBottomTabNavigator();
 
+//TODO: replace with imports
 const phBuyScreen = () => (
   <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <Text category='h1'>Buy</Text>
@@ -18,24 +23,19 @@ const phRequestScreen = () => (
   </Layout>
 );
 
-/*export const TabNavigator = () => (
-<NavigationContainer>
-  <Stack.Navigator>
-    <Stack.Screen name='Home' component={ Login }/>
-    <Stack.Screen name='Your Communities' component={ CommunitiesNav }/>
-    <Stack.Screen name='Map' component={ mapScreen }/>
-  </Stack.Navigator>
-  </NavigationContainer>
-);*/
+export const mapScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text category='h1'>MAP</Text>
+  </Layout>
+);
 
-export function Tabs() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Your Communities" component={YourCommunitiesScreen} />
-        <Tab.Screen name="Buy" component={phBuyScreen} />
-        <Tab.Screen name="Request" component={phRequestScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
+export default Tabs = () => (
+<NavigationContainer independent={true}>
+  <Tab.Navigator>
+  <Tab.Screen name="Your Communities" component={CommunitiesNav} />
+  <Tab.Screen name="Buy" component={phBuyScreen} />
+  <Tab.Screen name="Request" component={phRequestScreen} />
+  <Tab.Screen name="Map" component={mapScreen} />
+  </Tab.Navigator>
+  </NavigationContainer>
+);
