1b92e95e9db0729373fcc2c918f4cb8dd3485b5a
selenaqian <selenaq2010@gmail.com>
2020-04-26 05:27:59 -0400

    styling for communities screen, example communities

--- frontend/App.js
+++ frontend/App.js
@@ -5,12 +5,6 @@ import * as eva from '@eva-design/eva';
 import {ApplicationProvider, Layout, Text} from '@ui-kitten/components';
 import {AppNavigator} from './navigation/AppNavigator';
 
-const HomeScreen = () => (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category="h1">HOME</Text>
-  </Layout>
-);
-
 export default () => (
     <ApplicationProvider {...eva} theme={eva.light}>
       <AppNavigator/>
--- frontend/screens/Communities.js
+++ frontend/screens/Communities.js
@@ -3,21 +3,69 @@
 
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text, Button } from '@ui-kitten/components';
+import { StyleSheet, View } from 'react-native';
+import { ApplicationProvider, Layout, Text, Button, Divider, Card } from '@ui-kitten/components';
 import {  TouchableOpacity } from 'react-native'
 
 import Login from '../components/Drawer/Login/Login.js';
 import TabNavigator from '../navigation/TabNavigator.js';
 
+const Neighborhood = (props) => (
+  <Layout {...props} style={styles.cardHeader}>
+    <Text category='h6'>Neighborhood</Text>
+  </Layout>
+);
+
+const Friends = (props) => (
+  <Layout {...props} style={styles.cardHeader}>
+    <Text category='h6'>Friends</Text>
+  </Layout>
+);
+
 class Communities extends Component {
   render () {
     return (
-      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Your Communities</Text>
+      <Layout style={styles.backgroundContainer}>
+        <Text category='h1'>Your Communities</Text>
+        <Card style={styles.card} header={Neighborhood}>
+            <Text>
+              Requests made: 3
+            </Text>
+            <Text>
+              No current planned purchases.
+            </Text>
+          </Card>
+          <Card style={styles.card} header={Friends}>
+              <Text>
+                Requests made: 7
+              </Text>
+              <Text>
+                Upcoming trip: 4:00 p.m. April 26, 2020 (today)
+              </Text>
+            </Card>
       </Layout>
       )
   }
 }
 
 export default Communities
+
+const styles = StyleSheet.create({
+  backgroundContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  card:{
+    backgroundColor: '#ee8277',
+    fontSize: 35,
+    width: 350,
+    marginTop: 30,
+  },
+  cardHeader:{
+    backgroundColor: '#ee8277',
+    marginLeft: 20,
+    marginTop: 10,
+    marginBottom: 10
+  }
+});
